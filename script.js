da503347336392c526183477484fc5f20672abe0
Wally132 <wallacechen132@gmail.com>
2024-05-26 15:02:06 -0700

    Finished black etch a sketch - Rainbow color soon

--- script.js
+++ script.js
@@ -1,26 +1,69 @@
-// Make a function to create 16x16 grid - This is the default size.
+//Start with DOMContentLoaded to make sure the script runs after the DOM is fully loaded.
+//Make function for 16x16 sqaures inside container.
+// 
 
-const changeSizeBtn = document.querySelector('#grid-size-btn');
-const container = document.querySelector('#container');
-let size = 16;
+// DOMContentLoaded allows function to be called after DOM is loaded.
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('container')
+//Creating variable 'size' is 16 to start.
+    let size = 16;
 
-// function creates grid with prompted size.
+function randomColor() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    const newColor = 'rgb(' + r + "," + g + "," + b +')';
+    console.log(newColor);
+}
 
+randomColor();
+
+
+// Function - 2 loops
 function createGrid(size) {
-    container.innerHTML = ''; // Clear previous grid
-
+    container.innerHTML = ''; // Apparently this clears the previous grid.
     for (let i = 0; i < size; i++) {
-        const row = document.createElement('div');
-        row.classList.add('row');
+        const row = document.createElement('div'); // Creates divs inside container.
+        row.classList.add('row'); // Adds class to rows.
+        container.appendChild(row); // Appends row to container.
 
-        for (let j = 0; j < size; j++) {
-            const divBox = document.createElement('div');
-            divBox.classList.add('box');
-            row.appendChild(divBox);
-        }
+    for (let j = 0; j < size; j++) {
+        const column = document.createElement('div');
+        column.classList.add('column');
+        row.appendChild(column); // Appends column divs to each row div.
 
-        container.appendChild(row);
+    function blackInk() {
+        column.style.backgroundColor = '#000000';
+    }
+
+   
+
+    column.addEventListener('mouseover', blackInk);
+    }
     }
 }
 
+function changeSize() {
+    const gridSize = prompt('1-100');
+    if (gridSize > 100 || gridSize < 1) {
+        alert('Hey Dumbass');
+    } else {
+        createGrid(gridSize);
+    }
+}
+
+function resetGrid() {
+    createGrid(16);
+}
+
+
 createGrid(size);
+
+const gridChange = document.getElementById('grid-size-btn');
+gridChange.addEventListener('click', changeSize);
+
+const resetButton = document.getElementById('reset-btn');
+resetButton.addEventListener('click', resetGrid);
+});
+
+
